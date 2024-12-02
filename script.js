20da031db4e5ee1c0b387ae04a544347d3399988
Kelistof <yjsp1104@163.com>
2024-12-02 22:25:19 +0800

    Joke is Update! :)
    no way

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ function updateClock() {
 // 初始化时钟并设置间隔
 setInterval(updateClock, 1000);
 updateClock();// JavaScript Document
+document.addEventListener("DOMContentLoaded", () => {
+    const digitalArtMuseumButton = document.getElementById("digital-art-museum");
+    const goBackButton = document.getElementById("go-back");
+
+    if (digitalArtMuseumButton) {
+        digitalArtMuseumButton.addEventListener("click", () => {
+            window.location.href = "museum.html";
+        });
+    }
+
+    if (goBackButton) {
+        goBackButton.addEventListener("click", () => {
+            window.location.href = "index.html";
+        });
+    }
+});
